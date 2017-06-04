12426bd55df47f3baf9c4cee39eb1dd6ade6b1ac
Matthew Casperson <matthewcasperson@gmail.com>
2017-06-05 05:37:27 +1000

    Removed infrastructure scripts

--- infrastructure/roles/bamboo5agent/templates/runagent.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -jar /opt/atlassian-bamboo-agent-installer-5.15.7.jar http://${1}:8085/agentServer/
--- infrastructure/run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ansible-playbook -i ec2.py --private-key /mnt/c/Users/matth/Downloads/OctopusVirginia.pem bamboo.yml
