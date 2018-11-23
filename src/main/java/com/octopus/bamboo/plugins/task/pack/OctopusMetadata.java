1ae0b259d4ddb5e4198b83b87bf4c082891f446a
Shannon Lewis <slewis74@gmail.com>
2018-11-23 17:03:22 +1000

    added BuildServerType to the OctopusMetadata

--- src/main/java/com/octopus/bamboo/plugins/task/pack/OctopusMetadata.java
+++ src/main/java/com/octopus/bamboo/plugins/task/pack/OctopusMetadata.java
@@ -3,5 +3,10 @@ package com.octopus.bamboo.plugins.task.pack;
 import java.util.List;
 
 public class OctopusMetadata {
+    public String BuildServerType;
     public List<WorkItem> WorkItems;
+
+    public OctopusMetadata() {
+        BuildServerType = "Bamboo";
+    }
 }
