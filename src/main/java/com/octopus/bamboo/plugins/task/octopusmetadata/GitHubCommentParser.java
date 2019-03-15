c78c69af06d8c2db0bacaf4a8a0608fbbd52dd55
Henrik Andersson <henrik.andersson@octopus.com>
2019-03-15 15:56:28 +1000

    Refactor regex to only include issues that are closed as part of a commit

--- src/main/java/com/octopus/bamboo/plugins/task/octopusmetadata/GitHubCommentParser.java
+++ src/main/java/com/octopus/bamboo/plugins/task/octopusmetadata/GitHubCommentParser.java
@@ -8,7 +8,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GitHubCommentParser extends CommentParser {
-    private static final String GITHUB_ID_REGEX = "(?:([/A-Z]*)#|GH-|http[/A-Z:.]*/issues/)(\\d+)";
+    private static final String GITHUB_ID_REGEX = "(?:close[d|s]*|fix[ed|es]*|resolve[d|s]*):?\\s((?:#|GH-|http[/A-Z:.]*/issues/)(\\d+))";
 
     public String getIssueTrackerSuffix() {
         return "github";
@@ -18,12 +18,12 @@ public class GitHubCommentParser extends CommentParser {
         buildLogger.addBuildLogEntry("Parsing comments for GitHub issues");
         final List<WorkItem> workItems = new ArrayList<WorkItem>();
 
-        final Pattern githubId = Pattern.compile(GITHUB_ID_REGEX);
+        final Pattern githubId = Pattern.compile(GITHUB_ID_REGEX, Pattern.CASE_INSENSITIVE);
         final Matcher githubMatcher = githubId.matcher(comment);
 
         while (githubMatcher.find()) {
             final WorkItem workItem = new WorkItem();
-            final String linkData = githubMatcher.group(0);
+            final String linkData = githubMatcher.group(1);
             final String issueNumber = githubMatcher.group(2);
 
             workItem.Id = issueNumber;
